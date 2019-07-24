d2baa2e29cd357ff983659ad30c3ef87468ec757
onyas <zheng0518@163.com>
2019-07-24 16:40:37 +0800

    :ok_hand: works when using url

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	cookieJar, err := browsercookie.LoadCookieJarFromChrome("")
+	cookieJar, err := browsercookie.LoadCookieJarFromChrome("https://bing.com")
 	if err != nil {
 		log.Fatal(err)
 	}
